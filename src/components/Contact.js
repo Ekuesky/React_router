6ad636ac842e4ddfa2312fe1c5962fa00ab90b04
Ekuesky <ayiekue9127@gmail.com>
2024-09-18 16:24:54 +0000

    training on react-router

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = ()=>{
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+                <p className="green-text">Possim dolores vestibulum nibh alterum qualisque fringilla. Non aliquam dico convenire nisl tristique theophrastus parturient oporteat decore. Litora adipiscing eos fastidii efficitur mauris enim persecuti ancillae. Tempus pulvinar eleifend voluptatum sem gloriatur. Fabellas senserit fames facilis ei natum similique vituperata nonumy.
+                </p>
+        </div>
+    )
+}
+
+export default Home
